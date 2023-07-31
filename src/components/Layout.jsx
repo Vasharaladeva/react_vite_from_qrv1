9e094abdc88a2c5d125bb6e50b51baedc0aa8137
Vasharaladeva <51453264+Vasharaladeva@users.noreply.github.com>
2023-07-31 14:07:17 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet, Link, useLocation } from 'react-router-dom'
+import img from '../../public/logo.jpeg'
 
 function Layout() {
     const location = useLocation()
@@ -6,7 +7,18 @@ function Layout() {
         <div className='md:flex md:min-h-screen'>
             <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
 
-                <h2 className='text-4xl font-black text-center text-white'>Travelero</h2>
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="block h-8 w-auto lg:hidden"
+                    src={img}
+                    alt="Your Company"
+                  />
+                  <img
+                    className="hidden h-8 w-auto lg:block"
+                    src={img}
+                    alt="Your Company"
+                  />
+                </div>
                 <nav className='mt-10'>
                     <Link
                         className={`${location.pathname === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300 `}
