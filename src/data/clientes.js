224fe80058f96ddc620052d7054f27d257be884c
Vasharaladeva <51453264+Vasharaladeva@users.noreply.github.com>
2023-08-02 12:24:19 -0400

    Update clientes.js

--- src/data/clientes.js
+++ src/data/clientes.js
@@ -10,43 +10,4 @@ export async function obtenerCliente(id) {
     return resultado
 }
 
-export async function agregarCliente(datos) {
-    try {
-        const respuesta = await fetch(import.meta.env.VITE_API_URL, {
-            method: 'POST',
-            body: JSON.stringify(datos),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        await respuesta.json()
-    } catch (error) {
-        console.log(error)
-    }
-}
 
-export async function actualizarCliente(id, datos) {
-    try {
-        const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify(datos),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        await respuesta.json()
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export async function eliminarCliente(id) {
-        try {
-        const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {
-            method: 'DELETE'
-        })
-        await respuesta.json()
-    } catch (error) {
-        console.log(error)
-    }
-}
