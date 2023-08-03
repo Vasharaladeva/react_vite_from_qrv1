7222a7a374a797efbd65d743eac08a0fcae7f89d
Vasharaladeva <51453264+Vasharaladeva@users.noreply.github.com>
2023-08-03 00:25:23 -0400

    Update clientes.js

--- src/data/clientes.js
+++ src/data/clientes.js
@@ -1,11 +1,11 @@
 export async function obtenerClientes() {
-    const respuesta = await fetch('http://52.87.247.113/estudiante')
+    const respuesta = await fetch('https://52.87.247.113/estudiante')
     const resultado = await respuesta.json()
     return resultado
 }
 
 export async function obtenerCliente(id) {
-    const respuesta = await fetch(`http://52.87.247.113/estudiante/${id}`)
+    const respuesta = await fetch(`https://52.87.247.113/estudiante/${id}`)
     const resultado = await respuesta.json()
     return resultado
 }
